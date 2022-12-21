df12d28d60f5807daccdcd8e90626a02f49215ce
galaxygcodergirl <101486713+galaxygcodergirl@users.noreply.github.com>
2022-12-21 13:37:50 +1100

    Add files via upload

---
+++ bookshelf.js
@@ -0,0 +1,45 @@
+img = "";
+status = "";
+objects = [];
+function preload(){
+    img = loadImage("bookshelf.webp");
+}
+function setup(){
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function draw(){
+    image(img, 0, 0, 640, 420);
+}
+function modelLoaded(){
+    console.log("Model Loaded");
+    status = true;
+    objectDetector.detect(img, gotResults);
+}
+function gotResults(error, results){
+    if (error){
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+function draw(){
+    image(img, 0, 0, 640, 420);
+    
+    if (status != ""){
+    
+        for(i=0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status : Objects Detected";
+
+            percent = floor(objects[i].confidence * 100);
+            fill("red");
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("red");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
